5a43304a0cb97df0acfca4666499c8170c1a1be7
Jose C Quiroga <josecquiroga@gmail.com>
2016-02-19 13:26:02 +0100

    -•◘•-

---
+++ WorkerRole1/Correo.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace WorkerRole1
+{
+    public class Correo
+    {
+        public string Origen { get; set; }
+        public string Destino { get; set; }
+        public string Asunto { get; set; }
+        public string Contenido { get; set; }
+    }
+}
